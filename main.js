88e5f21b078edcd7eb8579d55d941d82f8647b78
Robert <raduturobert20@gmail.com>
2023-04-24 00:01:26 +0300

    changes

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.mod
 
 import {FBXLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/FBXLoader.js';
 import {GLTFLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/GLTFLoader.js';
-import { Actors } from './actors';
 
 
 class BasicCharacterControllerProxy {
@@ -594,15 +593,43 @@ class ThirdPersonCameraDemo {
 
     let monsters = [];
     this._LoadAnimatedModel();
-    // this._LoadMonster(20, 0, 0);
-    // this._LoadMonster(20, 0, 10);
-    // this._LoadMonster(20, 0, 20);
-    // this._LoadMonster(20, 0, 30);
-    // this._LoadMonster(-20, 0, 0);
-    // this._LoadMonster(-20, 0, 10);
-    // this._LoadMonster(-20, 0, 20);
-    // this._LoadMonster(-20, 0, 30);
+    this._LoadMonster(20, 0, 0);
+    this._LoadMonster(20, 0, 10);
+    this._LoadMonster(20, 0, 20);
+    this._LoadMonster(20, 0, 30);
+    this._LoadMonster(-20, 0, 0);
+    this._LoadMonster(-20, 0, 10);
+    this._LoadMonster(-20, 0, 20);
+    this._LoadMonster(-20, 0, 30);
     this._RAF();
+  }
+
+  _LoadMonster(x, y, z){
+    const ms = new FBXLoader();
+    ms.setPath('./resources/zombie/');
+    ms.load('gana.fbx', (fbx) => {
+      fbx.scale.setScalar(0.1);
+      fbx.traverse(c => {
+        c.castShadow = true;
+      });
+
+      const anim = new FBXLoader();
+      anim.setPath('./resources/zombie/');
+      anim.load('bow.fbx', (anim) => {
+        const m = new THREE.AnimationMixer(fbx);
+        this._mixers.push(m);
+        const idle = m.clipAction(anim.animations[0]);
+        idle.play();
+        this._scene.add(anim);
+      });
+
+      this._target = fbx;
+      this._target.position.x = x;
+      this._target.position.y = y;
+      this._target.position.z = z;
+      this._scene.add(this._target);
+    });
+
   }
 
   _LoadAnimatedModel() {
@@ -657,8 +684,6 @@ let _APP = null;
 
 window.addEventListener('DOMContentLoaded', () => {
   _APP = new ThirdPersonCameraDemo();
-  _ACTORS = new Actors();
-  _ACTORS._LoadMonster(-20, 0 ,0);
 });
 
 
